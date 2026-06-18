5fe2ef21911860ad2b047180d840863218117470
Hridoy Chowdhury <t.okay383@gmail.com>
2026-06-19 00:55:43 +0600

    forum POST and GET api added

--- index.js
+++ index.js
@@ -34,6 +34,66 @@ async function run() {
         console.log("Connected Successfully to MongoDB!");
 
 
+        // NO: 01 - [POST] Add forum post - admin and trainer dashboard api
+        app.post('/api/forum-posts', async (req, res) => {
+            try {
+                const { title, image, description, authorName, authorEmail, authorRole } = req.body;
+
+                const newPost = {
+                    title,
+                    image,
+                    description,
+                    authorName,
+                    authorEmail,
+                    authorRole,
+                    like: [],
+                    dislike: [],
+                    createdAt: new Date()
+                }
+
+                const result = await forumPostCollection.insertOne(newPost);
+
+                res.status(201).send({
+                    success: true,
+                    message: 'Post Successfully Added in forum'
+                })
+
+            } catch (error) {
+                res.status(500).send({
+                    message: "Forum Post Unsuccessful",
+                    error: error.message
+                })
+            }
+        })
+
+        // NO: 02 [GET] Community Forum Page with Server-side Pagination (Public) 
+        app.get('/api/forum-posts', async (req, res) => {
+            try {
+
+                const page = parseInt(req.query.page) || 1;
+                const limit = parseInt(req.query.limit) || 6;
+                const skip = (page - 1) * limit;
+
+                const totalPosts = await forumPostCollection.countDocuments()
+
+                const forumPostsData = await forumPostCollection.find().sort({ createdAt: -1 }).skip(skip).limit(limit).toArray()
+
+                res.status(201).send({
+                    success: true,
+                    message: 'forum post data fatched successfully',
+                    totalPosts,
+                    totalPage: Math.ceil(totalPosts / limit),
+                    data: forumPostsData
+                })
+
+            } catch (error) {
+                res.status(500).send({
+                    success: false,
+                    message: 'forum post fetched failed',
+                    error: error.message
+                })
+            }
+        })
 
 
 
@@ -50,11 +110,6 @@ async function run() {
 
 
 
-
-
-
-
-        
     } finally {
 
     }
